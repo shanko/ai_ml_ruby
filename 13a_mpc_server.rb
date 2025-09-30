8a64a11f389cf07088366d26edaf771086013d8b
Shashank Date <stdate@gmail.com>
2025-09-30 14:25:13 -0500

    Document the 13a_mpc_server to show how to invoke it

--- 13a_mpc_server.rb
+++ 13a_mpc_server.rb
@@ -12,3 +12,9 @@ end
 # Create and start the transport
 transport = MCP::Server::Transports::StdioTransport.new(server)
 transport.open
+
+# Client request
+# {"jsonrpc":"2.0","id":"0","method":"ping"}
+# {"jsonrpc":"2.0","id":"1","method": "add", "params": { "a": 5, "b": 3 } }
+# {"jsonrpc":"2.0","id":"2","method":"tools/list"}
+# {"jsonrpc":"2.0","id":"3","method":"tools/call","params":{"name":"add","arguments":{ "a": 5, "b": 3 }}}
